2e25a35642c3b1859fde552289f1474f661eed7c
kyosukeeeeee <mikeneko3511@gmail.com>
2024-08-03 21:44:45 +0900

    20240803

--- react-guide-material/react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ react-guide-material/react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,7 +1,31 @@
+import { useState, useReducer } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, {type, step}) => {
+    if(type === '+') {
+      return prev + step;
+    } else if(type === '-') {
+      return prev - step;
+    }
+  }, 0)
+
+  const countUp = () => {
+    setState(prev => ++prev);
+  }
+  const rcountUp = () => {
+    dispatch({ type: '+', step: 2 });
+  }
+  const rcountDown = () => {
+    dispatch({ type: '-', step: 3 });
+  }
   return (
     <>
+      <h3>{state}</h3>
+      <button onClick={countUp}>+</button>
+      <h3>{rstate}</h3>
+      <button onClick={rcountUp}>+</button>
+      <button onClick={rcountDown}>-</button>
     </>
   );
 };
--- react-guide-material/react-guide-material/11_hooks_p1/src/025_practice_useReducer/start/Example.jsx
+++ react-guide-material/react-guide-material/11_hooks_p1/src/025_practice_useReducer/start/Example.jsx
@@ -1,8 +1,29 @@
 import { useReducer } from "react";
 
-const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
+const CALC_OPTIONS = ["add", "minus", "divide", "divide"];
 
-const reducer = () => {}
+const reducer = (state,{type, payload}) => {
+  switch(type) {
+    case "change": {
+      const {name, value} = payload
+      return {
+        ...state, [name]: value
+      };
+    }
+    case "add": {
+      return { ...state, result: Number(state.a) + Number(state.b) }
+    }
+    case "minus": {
+      return { ...state, result: Number(state.a) - Number(state.b) }
+    }
+    case "divide": {
+      return { ...state, result: Number(state.a) * Number(state.b) }
+    }
+    case "divide": {
+      return { ...state, result: Number(state.a) / Number(state.b) }
+    }
+  }
+}
 
 const Example = () => {
   const initState = {
@@ -11,41 +32,36 @@ const Example = () => {
     result: 3,
   };
 
-  const [state, dispatch] = useReducer(reducer, initState);
+  const [state, dispatch] = useReducer(reducer,initState);
 
   const calculate = (e) => {
-    
+    dispatch({ type: e.target.value });
   };
 
   const numChangeHandler = (e) => {
-    
+    dispatch(
+      {
+        type: "change",
+        payload: {name: e.target.name, value: e.target.value}
+      }
+    )
   }
 
   return (
     <>
-    <h3>練習問題</h3>
-    <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
-      {/* <div>
-        a:
-        <input
-          type="number"
-          name="a"
-          value={state.a}
-          onChange={numChangeHandler}
-        />
+      <div>
+        <label htmlFor="">a:<input name="a" value={state.a} type="number" onChange={numChangeHandler} /></label>
       </div>
       <div>
-        b:
-        <input
-          type="number"
-          name="b"
-          value={state.b}
-          onChange={numChangeHandler}
-        />
+        <label htmlFor="">b:<input name="b" value={state.b} type="number" onChange={numChangeHandler} /></label>
       </div>
+
       <select value={state.type} onChange={calculate}>
+        {CALC_OPTIONS.map((calc) => {
+          return <option key={calc} value={calc}>{calc}</option>
+        })}
       </select>
-      <h1>結果：{state.result}</h1> */}
+      <h3>{state.result}</h3>
     </>
   );
 };
